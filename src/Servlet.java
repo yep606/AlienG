20c940dcb291a56ccc536637b90167b7e8809573
yep606 <ilya.rogov.vladislav@gmail.com>
2020-01-25 23:54:45 +0300

    Setup and run Tomcat, create 1st servlet

---
+++ src/Servlet.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+
+        pw.println("<html>");
+        pw.println("<h1>Hello world! </h1>");
+        pw.println("</html>");
+    }
+}
