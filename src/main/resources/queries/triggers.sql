32883e7f8a67688a938819aaca092a35d2826da3
yep606 <ilya.rogov.vladislav@gmail.com>
2020-05-27 18:52:40 +0300

    sql stuff

--- src/main/resources/queries/triggers.sql
+++ src/main/resources/queries/triggers.sql
@@ -48,5 +48,49 @@ CREATE OR REPLACE FUNCTION after_func_delete() RETURNS TRIGGER AS $$
     END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER task_AFTER_DELETE AFTER DELETE ON task FOR EACH ROW EXECUTE PROCEDURE after_func_delete();
+
+--AFTER UPDATE
+
+CREATE OR REPLACE FUNCTION after_func_update() RETURNS TRIGGER AS $$
+    BEGIN
+        if(coalesce(OLD.completed,0) <> coalesce(NEW.completed,0) AND NEW.completed=1 AND coalesce(OLD.category_id,0) = coalesce(NEW.category_id,0) )
+        then
+            UPDATE category SET uncompleted_count = (coalesce(uncompleted_count,0)-1), completed_count = (coalesce(completed_count,0)+1) WHERE id = OLD.category_id;
+            UPDATE stat SET uncompleted_total = (coalesce(uncompleted_total,0)-1), completed_total = (coalesce(completed_total,0)+1) WHERE id=1;
+        end if;
+
+        if(coalesce(OLD.completed,0) <> coalesce(NEW.completed,0) AND NEW.completed=0 AND coalesce(OLD.category_id,0) = coalesce(NEW.category_id,0) )
+        then
+            UPDATE category SET uncompleted_count = (coalesce(uncompleted_count,0)+1), completed_count = (coalesce(completed_count,0)-1) WHERE id = OLD.category_id;
+            UPDATE stat SET uncompleted_total = (coalesce(uncompleted_total,0)+1), completed_total = (coalesce(completed_total,0)-1) WHERE id=1;
+        end if;
+
+
+
+
+        /*Changed category for completed=1 */
+        if(coalesce(OLD.completed,0) = coalesce(NEW.completed,0) AND NEW.completed=1 AND coalesce(OLD.category_id,0) <> coalesce(NEW.category_id,0) )
+        then
+            UPDATE category SET completed_count = (coalesce(completed_count,0)-1) WHERE id = OLD.category_id;
+            UPDATE category SET completed_count = (coalesce(completed_count,0)+1) WHERE id = NEW.category_id;
+        end if;
+
+
+
+
+        /*Changed category for completed=0 */
+         if(coalesce(OLD.completed,0) = coalesce(NEW.completed,0) AND NEW.completed=0 AND coalesce(OLD.category_id,0) <> coalesce(NEW.category_id,0) )
+        then
+            UPDATE category SET uncompleted_count = (coalesce(uncompleted_count,0)-1) WHERE id = OLD.category_id;
+            UPDATE category SET uncompleted_count = (coalesce(uncompleted_count,0)+1) WHERE id = NEW.category_id;
+        end if;
+
+        /*Changed category, changed completed from 1 to 0 */
+
+        /*Changed category, changed completed from 0 to 1 */
+
+
+
 
 
