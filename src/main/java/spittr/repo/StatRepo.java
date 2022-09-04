49116dfd407142fd771537e0ef3f2e099997d97b
yep606 <ilya.rogov.vladislav@gmail.com>
2022-09-04 03:04:05 +0300

    Update :)

--- src/main/java/spittr/repo/StatRepo.java
+++
@@ -1,9 +0,0 @@
-package spittr.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import spittr.entity.Stat;
-
-@Repository
-public interface StatRepo extends CrudRepository<Stat, Long> {
-}
--- src/main/java/spittr/repo/TaskRepo.java
+++
@@ -1,9 +0,0 @@
-package spittr.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import spittr.entity.Task;
-
-@Repository
-public interface TaskRepo extends JpaRepository<Task, Long> {
-}
