d60e2384023951930636bd336ebd57eb7775790c
yep606 <ilya.rogov.vladislav@gmail.com>
2022-09-18 18:52:27 +0300

    Aliens movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -101,11 +102,27 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_bullets(self):
         self.bullets.update()
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -17,3 +17,9 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
+        # Aliens settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
+
